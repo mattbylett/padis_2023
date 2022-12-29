6fa54968171f04c5f93f318fee7b27ea56f2b04b
mattbylett <31151177+mattbylett@users.noreply.github.com>
2022-12-30 08:25:26 +1300

    debbing custitem42

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -396,22 +396,22 @@ class ProductController extends BaseController
             }
         }
 
-        // if (isset($result->custitem42)) {
-        //     if ($result->custitem42) {
-        //         $p_promote = "Home Page - Featured";                
-        //         $weekly_specials_insinc = 124022;
-        //     }
-        // }
-
-        $promotions = [];
-        if (isset($result->custitem42)){
-            if($result->cusitem42) {
-                $promotions = [
-                    "DeleteMissingArrayElements" => true,
-                    "promo_tag" => "Home Page - Featured"
-                ];
+        if (isset($result->custitem42)) {
+            if ($result->custitem42) {
+                $p_promote = "Home Page - Featured";                
+                $weekly_specials_insinc = 124022;
             }
         }
+
+        // $promotions = [];
+        // if (isset($result->custitem42)){
+        //     if($result->cusitem42) {
+        //         $promotions = [
+        //             "DeleteMissingArrayElements" => true,
+        //             "promo_tag" => "Home Page - Featured"
+        //         ];
+        //     }
+        // }
 
         $p_tab_video = "";
         if (isset($result->custitem33)) {
