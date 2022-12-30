0dda3df22605ef31d529a6446956328ebce322c4
mattbylett <31151177+mattbylett@users.noreply.github.com>
2022-12-31 10:10:18 +1300

    Cleaning up Xero Controller

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ImportController;
+use App\Http\Controllers\API\XeroController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -52,7 +53,7 @@ Route::prefix('admin')->group(function()
  * We name this route xero.auth.success as by default the config looks for a route with this name to redirect back to
  * after authentication has succeeded. The name of this route can be changed in the config file.
  */
-Route::get('/manage/xero', [\App\Http\Controllers\XeroController::class, 'index'])->name('xero.auth.success');
+Route::get('/manage/xero', [XeroController::class, 'index'])->name('xero.auth.success');
 
 
 /** ___________________-END ADMIN ROUTES-_____________________________________ */
