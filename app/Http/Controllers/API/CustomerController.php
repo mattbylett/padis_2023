ebfccdf69dd4c8587d7a1ab1087f33850059876d
mattbylett <31151177+mattbylett@users.noreply.github.com>
2023-08-21 11:12:22 +1200

    Adding Customer Controller --fix use statement

--- app/Http/Controllers/API/CustomerController.php
+++ app/Http/Controllers/API/CustomerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Netsuite\NetsuiteApi;
+use App\Netsuite\NetSuiteApi;
 use Illuminate\Support\Facades\Log;
 
 class CustomerController extends Controller
