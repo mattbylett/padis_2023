81253018c7fb787ccbf4cac959c15fa4043fc893
mattbylett <31151177+mattbylett@users.noreply.github.com>
2023-08-22 10:55:34 +1200

    Making the Call To Website World

--- app/Http/Controllers/API/CustomerController.php
+++ app/Http/Controllers/API/CustomerController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\WebsiteId;
 use App\NetSuite\NetSuiteApi;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class CustomerController extends Controller
 {
@@ -53,8 +55,36 @@ class CustomerController extends Controller
         Log::info('Getting The Email To Search For Customer in API');
         Log::debug(json_encode($mbrEmail));
 
+       // Connect to Website World
+        $http_insinc = Http::withHeaders([
+            "apiID" => config("services.website.insinc_api_id"),
+            "apiKey" => config("services.website.insinc_api_key"),
+        ]);
+
+        $base_uri = config("services.website.base_uri");
+       
+        // call The API and see if the Email Exists - Fetch ID
+        try {
+            $response = $http_insinc->get(
+                "{$base_uri}/member", $mbrEmail
+            );
+            $customer = $response->json();
+
+            Log::info('Call Made To Website World.  Logging Customer');
+            Log::debug($customer);
+
+        // If Email Exists - then Update (Include Id)
+
+
+        // If email Doesnt exist - then Create (same request - no ID)
+
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+        }
+
+
         
-        return $customerData;
+        return;
     }
 
     public function show($id)
