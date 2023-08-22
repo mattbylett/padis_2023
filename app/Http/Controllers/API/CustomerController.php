b9da6181f02fa832fbfc15a4c37dd2f2d1265f92
mattbylett <31151177+mattbylett@users.noreply.github.com>
2023-08-22 16:17:41 +1200

    Making the Call To Website World - Changing URL

--- app/Http/Controllers/API/CustomerController.php
+++ app/Http/Controllers/API/CustomerController.php
@@ -84,12 +84,12 @@ class CustomerController extends Controller
         // call The API and see if the Email Exists - Fetch ID
         try {
             $response = $http_insinc->get(
-                "{$base_uri}/member", $mbrEmail
+                "{$base_uri}/member?=mbr_email=", $mbrEmail
             );
             $customer = $response->json();
 
             Log::info('Call Made To Website World.  Logging Customer');
-            // Log::debug($customer);
+            Log::debug($customer);
 
         // If Email Exists - then Update (Include Id)
 
