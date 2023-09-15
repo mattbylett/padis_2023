d6c499504ae05da5b60ebc3636522a3af8780bfd
mattbylett <31151177+mattbylett@users.noreply.github.com>
2023-09-15 07:59:08 -0700

    Debugging Group ID

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -1074,8 +1074,14 @@ public function featuredProducts(Request $request)
         $response = $httpInstance->get("{$base_uri}/products?p_code=" . $p_code);
         Log::info('Full GET URL: ' . "{$base_uri}/products?p_code=" . $p_code);
 
-        $result = $response->json();
-        Log::info('Get Request Response in processWebData: ', ['response' => $response]);
+        $result = json_decode($response->body(), true);
+        Log::info('Decoded Result: ', ['result' => $result]);
+        Log::info('Response type: ' . gettype($response));
+        Log::info('Response content: ' . $response->body());
+
+
+        // $result = $response->json();
+        // Log::info('Get Request Response in processWebData: ', ['response' => $response]);
 
 
         if ($type == "create" || (!isset($result["resultCount"]) || $result["resultCount"] == 0)) {
