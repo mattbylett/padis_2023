a0b103877f134b2a7060b4b4055aa6b9ed9b5749
mattbylett <31151177+mattbylett@users.noreply.github.com>
2023-10-16 11:49:12 +1300

    Debugging Bulk pricing fields

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -619,7 +619,8 @@ public function updateProduct(Request $request)
     function processWebsiteData($displayFlag, $httpInstance, $base_uri, $p_code, $data, $type, $groupId, $removeData, $additionalText = null)     
     {
         if ($displayFlag) {
-                $preparedData = $data; // Copying the data
+                $preparedData = json_encode($data); // Copying the data
+                // $preparedData = $data; // Copying the data
                 Log::debug('PreparedData: ', ['data' => $preparedData]);
                 // Get product details first  from Website World
                 $response = $httpInstance->get("{$base_uri}/product?p_code=" . $p_code);
