4c12943c69749918f3d2d4f91d7b051de9af321e
mattbylett <31151177+mattbylett@users.noreply.github.com>
2023-10-18 20:26:43 +1300

    Simplifying Logs

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -656,8 +656,8 @@ public function updateProduct(Request $request)
                 }
                 $response = $httpInstance->post("{$base_uri}/product", $preparedData);
                 $result = $response->json();
-
-                Log::debug('Result: ', ['Product Updated Successfully: ' => $result]);
+Log::debug('Result: ', ['Product Updated Successfully: ' => $result['data']['p_code']]);
+                //Log::debug('Result: ', ['Product Updated Successfully: ' => $result]);
                 // Log::info("Success for website with groupId: $groupId");
                 } else {
                     $response = $httpInstance->get("{$base_uri}/product?p_code=" . $p_code);
